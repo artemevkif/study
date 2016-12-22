b69c10d44e9a0049b2e05f9b483ab61e4d57c14d
artemevkif <ext.alexey.artemyev@tieto.com>
2016-12-22 12:39:47 +0300

    Создан проект для IDEA

---
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -0,0 +1,11 @@
+public class MyFirstProgram {
+	public static void main(String[] args) {
+		System.out.println("test1234567890-=!@#$%^&*                     _    абв    ?");
+		System.out.println("2string");
+		System.out.println("");
+		System.out.println("4");
+		System.out.println("  /--------------/");
+		System.out.println(" /      ня      /");
+		System.out.println("/--------------/");
+	}
+}
