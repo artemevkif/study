f36588861109ccf827ffa02e4f406071db516bb7
artemevkif <ext.alexey.artemyev@tieto.com>
2016-12-25 17:32:16 +0300

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,13 +1,13 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args) {
-		System.out.println("test1234567890-=!@#$%^&*                     _    абв    ?");
-		System.out.println("2string");
-		System.out.println("");
-		System.out.println("4");
-		System.out.println("  /--------------/");
-		System.out.println(" /      ня      /");
-		System.out.println("/--------------/");
-	}
+  public static void main(String[] args) {
+    System.out.println("test1234567890-=!@#$%^&*                     _    абв    ?");
+    System.out.println("2string");
+    System.out.println("");
+    System.out.println("4");
+    System.out.println("  /--------------/");
+    System.out.println(" /      ня      /");
+    System.out.println("/--------------/");
+  }
 }
