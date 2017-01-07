82cebe933082d77939f435d89b44266427c66d11
artemevkif <ext.alexey.artemyev@tieto.com>
2017-01-07 17:34:10 +0300

    Урок 1 - 14

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,16 +2,16 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    double l = 32;
-    double a = 4;
-    double b = 6;
 
     hello("324");
     hello("Aleksey");
     hello("world");
 
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+
+    Rectangle r = new Rectangle(14,6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 
   }
 
@@ -19,11 +19,11 @@ public class MyFirstProgram {
     System.out.println("Hello " + somebody + "!");
   }
 
-  public static double area (double len){
-    return len * len;
+  public static double area (Square s){
+    return s.l * s.l;
       }
 
-  public static double area (double a, double b){
-    return a * b;
+  public static double area (Rectangle r){
+    return r.a * r.b;
   }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by xxartema on 07.01.2017.
+ */
+public class Square {
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+}
