5fb5d888bba385b29e5c2dc2143cf8d159d215c9
artemevkif <ext.alexey.artemyev@tieto.com>
2017-01-07 18:39:03 +0300

    Задание №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,22 +1,28 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
+//  public static void main(String[] args) {
+//
+//    hello("324");
+//    hello("Aleksey");
+//    hello("world");
+//
+//    Square s = new Square(5);
+//    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+//
+//    Rectangle r = new Rectangle(14,6);
+//    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+//
+//  }
+//
+//  public static void hello(String somebody) {
+//    System.out.println("Hello " + somebody + "!");
+//  }
+
   public static void main(String[] args) {
+    Point p1 = new Point(1,0);
+    Point p2 = new Point(800,0);
 
-    hello("324");
-    hello("Aleksey");
-    hello("world");
-
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
-
-    Rectangle r = new Rectangle(14,6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-
+    System.out.println("Расстояние: " + p1.dist(p2));
   }
-
-  public static void hello(String somebody) {
-    System.out.println("Hello " + somebody + "!");
-  }
-
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -7,19 +7,13 @@ public class Point {
   public double a;
   public double b;
 
-  public static double distance (Point p1, Point p2) {
-    return Math.sqrt(Math.pow((p2.a - p1.a),2) + Math.pow((p2.b - p1.b),2));
+  public Point(double a, double b) {
+    this.a = a;
+    this.b = b;
   }
 
-  public static void main(String[] args) {
-    Point p1 = new Point();
-    p1.a = 1;
-    p1.b = 1;
-
-    Point p2 = new Point();
-    p2.a = 8;
-    p2.b = 8;
-
-    System.out.println("Расстояние: " + distance(p1, p2));
+  public double dist (Point p) {
+    return Math.sqrt(Math.pow((p.a - a),2) + Math.pow((p.b - b),2));
   }
+
 }
