1cf6418af6a2df0e074777b1ba8a9196f5835dfa
artemevkif <ext.alexey.artemyev@tieto.com>
2017-01-07 16:50:41 +0300

    урок 1 - 10

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,11 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
+    int l = 8;
+    int s = l * l;
+
+    System.out.println("Площадь " + l + " = " + s);
+    
     System.out.println("test1234567890-=!@#$%^&*                     _    абв    ?");
     System.out.println("2string");
     System.out.println("");
