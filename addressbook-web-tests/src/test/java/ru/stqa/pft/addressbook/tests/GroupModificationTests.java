2ebb5e42a86ec33cec0be9550c2ad3c019725550
artemevkif <ext.alexey.artemyev@tieto.com>
2017-01-30 18:23:43 +0300

    Урок 5 - 4

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -16,7 +16,7 @@ public class GroupModificationTests extends TestBase {
   public void ensurePreconditions() {
     app.goTo().groupPage();
     if (app.group().list().size() == 0) {
-      app.group().create(new GroupData("test1", null, null));
+      app.group().create(new GroupData().withName("test1"));
     }
   }
 
@@ -24,7 +24,11 @@ public class GroupModificationTests extends TestBase {
   public void testGroupModification() {
     List<GroupData> before = app.group().list();
     int index = before.size() - 1;
-    GroupData group = new GroupData(before.get(index).getId(), "qwe", null, null);
+    GroupData group = new GroupData()
+            .withId(before.get(index).getId())
+            .withName("qwe")
+            .withFooter("qwe")
+            .withHeader("qwe");
     app.group().modify(index, group);
     List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(), before.size());
