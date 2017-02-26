d2fa67728cb2002acb57577d84f9a9547808db99
artemevkif <ext.alexey.artemyev@tieto.com>
2017-02-26 22:39:47 +0300

    Урок 9 - 6

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("77cb9675bd094c19e5cecaef987b0c277d9ab9e5");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("artemevkif", "study")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
